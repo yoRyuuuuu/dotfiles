e3dec55333c0ba2a38b87fc3a76ca4f1e6f57a89
yoRyuuuuu <sasoseooo@gmail.com>
2022-06-20 11:10:43 +0900

    add config.fish

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/developer/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/developer/Downloads/google-cloud-sdk/path.fish.inc'; end
+
+export GO111MODULE=on 
+export PATH="$PATH:$(go env GOPATH)/bin" 
+export GOPATH=(go env GOPATH)
+export GOROOT=(go env GOROOT)
+
+export PATH="$PATH:/usr/local/opt/gnu-sed/libexec/gnubin"
+export PATH="$PATH:/usr/local/opt/gawk/bin"
+
+source ~/.cargo/env
