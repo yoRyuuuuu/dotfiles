07037c979bb0733e4ec71155614a48a3445f8ff8
yoRyuuuuu <sasoseooo@gmail.com>
2022-06-20 11:16:35 +0900

    update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,12 +5,15 @@ end
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/developer/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/developer/Downloads/google-cloud-sdk/path.fish.inc'; end
 
+# Go 
 export GO111MODULE=on 
 export PATH="$PATH:$(go env GOPATH)/bin" 
 export GOPATH=(go env GOPATH)
 export GOROOT=(go env GOROOT)
 
+# Rust
+source ~/.cargo/env
+
+# GNU tools
 export PATH="$PATH:/usr/local/opt/gnu-sed/libexec/gnubin"
 export PATH="$PATH:/usr/local/opt/gawk/bin"
-
-source ~/.cargo/env
